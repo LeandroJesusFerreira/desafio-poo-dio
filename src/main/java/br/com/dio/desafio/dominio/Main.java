b085401ac8b3e22b0989bdc46dd3ec622abf8c75
Leandro Jesus <101562981+LeandroJesusFerreira@users.noreply.github.com>
2023-06-06 07:37:26 -0300

    Classes Mentoria e Curso

---
+++ src/main/java/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,30 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descricao Curso Javasccript");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição Mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1.toString());
+        System.out.println(curso2.toString());
+        System.out.println(mentoria1.toString());
+
+    }
+}
